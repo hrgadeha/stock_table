dbccae29a93d708ae33c49e3fffb07b0deea2df0
Your Name <you@example.com>
2019-03-17 17:48:25 +0530

    changes in report

--- stock_table/stock_table/report/product_query_report/product_query_report.py
+++ stock_table/stock_table/report/product_query_report/product_query_report.py
@@ -11,42 +11,26 @@ def execute(filters=None):
 	return columns,data
 
 def get_column():
-	return [_("Item Code") + ":Data:180",_("Item Name") + ":Data:180",_("Discription") + ":Data:180",_("Sales Price") + ":Currency:100",_("Wholesale Price") + ":Currency:100",_("Last Sale Price") + ":Currency:100",_("Last Purchase Price") + ":Currency:100",_("SN Stock") + ":Float:100",_("AH Stock") + ":Float:100",_("MN Stock") + ":Float:100",_("SZ Stock") + ":Float:100",_("SH Stock") + ":Float:100"]
+	return [_("Item Code") + ":Link/Item:120",_("Item Name") + ":Data:180",_("Discription") + ":Data:180",_("UOM") + ":Data:50",_("Sales Price") + ":Currency:100",_("Wholesale Price") + ":Currency:100",_("Valuation Rate") + ":Currency:100",_("Last Purchase Price") + ":Currency:100",_("SN Stock") + ":Float:70",_("AH Stock") + ":Float:75",_("MN Stock") + ":Float:75",_("SZ Stock") + ":Float:70",_("SH Stock") + ":Float:70",_("Total Stock") + ":Float:70",_("Stock in Transit") + ":Float:100"]
 
 def get_data(filters):
 	if filters.get("item"):
 		item = filters.get("item")	
-		item_data = frappe.db.sql("""select item_code, item_name, description, standard_rate, wholesale_rate , 
-(select rate from `tabSales Invoice Item`,`tabSales Invoice` 
-where `tabSales Invoice Item`.parent = `tabSales Invoice`.name and item_code = '{0}'  
-order by `tabSales Invoice`.creation desc limit 1) AS 'Last Sale price',
+		item_data = frappe.db.sql("""select item_code, item_name, description,stock_uom, standard_rate,wholesale_rate,
+(select valuation_rate
+from `tabStock Ledger Entry` 
+where item_code =  '{0}' and is_cancelled='No' 
+order by posting_date desc, posting_time desc, 
+name desc limit 1),
 (select rate from `tabPurchase Invoice Item`,`tabPurchase Invoice` 
 where `tabPurchase Invoice Item`.parent = `tabPurchase Invoice`.name and item_code = '{0}'  
 order by `tabPurchase Invoice`.creation desc limit 1),
-(select qty_after_transaction 
-from `tabStock Ledger Entry` 
-where item_code =  '{0}' and warehouse = 'SN - B' and is_cancelled='No' 
-order by posting_date desc, posting_time desc, 
-name desc limit 1),
-(select qty_after_transaction 
-from `tabStock Ledger Entry` 
-where item_code =  '{0}' and warehouse = 'AH - B' and is_cancelled='No' 
-order by posting_date desc, posting_time desc, 
-name desc limit 1),
-(select qty_after_transaction 
-from `tabStock Ledger Entry` 
-where item_code =  '{0}' and warehouse = 'MN - B' and is_cancelled='No' 
-order by posting_date desc, posting_time desc, 
-name desc limit 1),
-(select qty_after_transaction 
-from `tabStock Ledger Entry` 
-where item_code =  '{0}' and warehouse = 'SZ - B' and is_cancelled='No' 
-order by posting_date desc, posting_time desc, 
-name desc limit 1),
-(select qty_after_transaction 
-from `tabStock Ledger Entry` 
-where item_code =  '{0}' and warehouse = 'SH - B' and is_cancelled='No' 
-order by posting_date desc, posting_time desc, 
-name desc limit 1)
-from `tabItem` where  item_code =  '{0}';; """.format(item), as_list=1)
+(select actual_qty from `tabBin` where item_code =  '{0}' and warehouse = 'SN - B'),
+(select actual_qty from `tabBin` where item_code =  '{0}' and warehouse = 'AH - B'),
+(select actual_qty from `tabBin` where item_code =  '{0}' and warehouse = 'MN - B'),
+(select actual_qty from `tabBin` where item_code =  '{0}' and warehouse = 'SZ - B'),
+(select actual_qty from `tabBin` where item_code =  '{0}' and warehouse = 'SH - B'),
+(select sum(actual_qty) from `tabBin` where item_code =  '{0}' and (warehouse = 'SN - B' or warehouse = 'SZ - B' or warehouse = 'AH - B' or warehouse = 'MN - B' or warehouse = 'SH - B')),
+(select ordered_qty from `tabBin` where item_code =  '{0}' order by name limit 1)
+from `tabItem` where  item_code =  '{0}'; """.format(item), as_list=1)
 		return item_data
